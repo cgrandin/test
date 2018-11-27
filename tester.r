b33a20fa60c12aecfd31d344ad8a977359172561
Chris Grandin <cgrandin@shaw.ca>
2018-11-27 14:34:41 -0800

    Add function to tester.r

--- tester.r
+++ tester.r
@@ -36,6 +36,44 @@ load.catches <- function(fn){
        landings.vs.tac = landings.vs.tac)
 }
 
+load.catches2 <- function(fn){
+  ## Reads in the catches file, splits it up into two data frames,
+  ##  one for catches and one for the catches/tac.
+  ## fn - the filename with relative path
+  ## In the catches data frame, any NA's will be replaced by zeroes.
+  ## In the landings/tac table, they will remain NAs
+  catches <- read.csv(fn)
+  landings.vs.tac <-
+    as.data.frame(cbind(Year = catches$Year,
+                        Ustotal = catches$Ustotal,
+                        CANtotal = catches$CANtotal,
+                        TOTAL = catches$TOTAL,
+                        TAC = catches$TAC,
+                        TACCAN = catches$TACCAN,
+                        TACUSA = catches$TACUSAXX))
+
+  landings.vs.tac <-
+    as.data.frame(cbind(landings.vs.tac,
+                        landings.vs.tac$Ustotal / landings.vs.tac$TACUSA * 100,
+                        landings.vs.tac$CANtotal / landings.vs.tac$TACCAN * 100,
+                        landings.vs.tac$TOTAL / landings.vs.tac$TAC * 100))
+
+  colnames(landings.vs.tac) <- c("Year",
+                                 "Ustotal",
+                                 "CANtotal",
+                                 "TOTAL",
+                                 "TAC",
+                                 "TACCAN",
+                                 "TACUSA",
+                                 "USATTAIN",
+                                 "CANATTAIN",
+                                 "ATTAIN")
+  catches <- catches[,!names(catches) %in% c("TAC", "TACCAN", "TACUSA")]
+  catches[is.na(catches)] <- 0
+  list(catches = catches,
+       landings.vs.tac = landings.vs.tac)
+}
+
 make.catches.table <- function(catches,
                                start.yr,
                                end.yr,
